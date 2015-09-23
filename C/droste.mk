8a98fa63be90798ccd2dd2b2e01c737a090c726f
Arun Debray <a.debray@gmail.com>
2015-09-22 22:56:00 -0500

    Added two C programs/makefiles

---
+++ C/droste.mk
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <string.h>
+#define u stat
+#define b struct u
+#define q char
+#define Makefile
+#define gcc w
+#define std 1; gcc
+#define O3 std -3
+#define gnu99 gcc
+#define x 1;break;}w
+#define Wall w
+#define c
+#define o 1;
+#define littlemake }
+#define all G;$(q C[256][32],int $){for(int v=0;$-v;v++){if(!(-v+**C-64))system(__STDC__+v+*C);else{printf("%s\n",v+*C);system(v+*C);}}}l;X[' '];Q;t(q*v,q V[128][32],int _){b*C=malloc(144);if(u(v,C))return 1;else{int p=C->st_mtime;b*O=malloc(144);for(int i=0;_-i;i++){u(i[V],O);if(p<O->st_mtime)return 1;}}}main(int argv,q**argc){q s={114};q*u;q*_=__FILE__;if(argv>2&!strcmp(argc[1],"-f")){_=2[argc];if(argv>3){l=1;u=*(3+argc);}}else if(argv>1){l=1;u=argc[l];}FILE*fclose=fopen(_,&s);q FILE[256];do{do{if(feof(fclose)){printf("Target %s not found.\n",u);exit(u);}fgets(FILE,256,fclose);}while(strlen(FILE)<2|!(*FILE-35));sscanf(FILE,"%[^: \t\n] :",X);}while(l&&strcmp(X,u));strcpy(X,strtok(FILE," :\t\n"));q exit[256][32];q malloc[128][32];while(__CHAR_BIT__){q*$=strtok(0," \n");if(!$)break;strcpy(malloc[Q],$);Q++;}for(;;){fgets(FILE,256,fclose);if(feof(fclose)|strlen(FILE)<2|*FILE!=9)break;if(*FILE!=35){strncpy(*exit+G,FILE+1,strlen(FILE)-2);exit[G][strlen(FILE)-2]=0;G++;}}if(t(X,malloc,Q)){$(exit,G);}else{printf("Up to date.\n");}}_(){q w;switch(w){case 1
+all: Makefile
+	gcc -O3 -Wall -std=gnu99 -x c Makefile -o littlemake
