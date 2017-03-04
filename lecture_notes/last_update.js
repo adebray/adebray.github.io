e4310b5a061888a0b2f0c19493f21f3fca8ae91a
Arun Debray <a.debray@gmail.com>
2017-03-04 17:26:54 -0600

    adding more update info to lecture_notes/

--- lecture_notes/last_update.js
+++ lecture_notes/last_update.js
@@ -1,6 +1,7 @@
 window.onload = function() {
 	
 	for (element of document.getElementsByClassName('updater')) {
-		element.innerHTML = 'Upated ' + all_files_info['./lecture_notes/' + element.getAttribute('filename')];
+		element.innerHTML = 'Upated '
+			+ all_files_info['./lecture_notes/' + element.getAttribute('filename')] + '.';
 	}
 }
