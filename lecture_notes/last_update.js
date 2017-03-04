db2397c97aa581c2f61ad5dd820549824370085b
Arun Debray <a.debray@gmail.com>
2017-03-04 17:14:21 -0600

    fixing JS mistakes

--- lecture_notes/last_update.js
+++ lecture_notes/last_update.js
@@ -1,5 +1,5 @@
 window.onload = function() {
 	for (element of document.getElementsByClassName('updater')) {
-		element.innerHTML = 'Upated ' + all_files_info['./lecture_notes/' + element.filename];
+		element.innerHTML = 'Upated ' + all_files_info['./lecture_notes/' + element.getAttribute(filename)];
 	}
 }
