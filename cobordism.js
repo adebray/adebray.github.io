c96ed15168218e247886a3929b7662d9f12af5db
Arun Debray <a.debray@gmail.com>
2017-06-29 15:54:28 -0500

    adding cobordism calculator

---
+++ cobordism.js
@@ -0,0 +1,48 @@
+/* This code uses a modified version of the partition-number algorithm to compute the
+   dimension of the F_2-vector space of unoriented coboridsm classes in a given dimension.
+ */
+
+// used for memoization
+var already_computed = {}
+
+// turns (n, cutoff) into a string, so it can be used as a key in a dict.
+const to_repr = function(i, j) {
+	return '' + i + ' ' + j;
+}
+
+/* Returns a set of numbers of the form 2^j - 1 (up to n), for ease of checking whether
+   a generator of the cobordism ring exists in a given dimension
+ */
+const not_generators = function(n) {
+	let no_gens = new Set();
+	for(let j = 0; j < 1 + Math.log2(n+1); j++) {
+		no_gens.add(Math.pow(2, j) - 1);
+	}
+	return no_gens;
+}
+
+const partitions_specified_by = function(no_gens, n, cutoff) {
+	if (n == 0) return 1;
+	if (n <  0) return 0;
+	const precomp = already_computed[to_repr(n, cutoff)]
+	if (precomp !== undefined) return precomp;
+	else {
+		let result = 0;
+		for(let k = 1; k <= cutoff; k++) {
+			if (!no_gens.has(k)) result += partitions_specified_by(no_gens, n-k, k);
+		}
+		already_computed[to_repr(n, cutoff)] = result;
+		return result;
+	}
+}
+
+// n is the dimension, so should be a nonnegative integer
+const dim_of_unoriented_cobordism_group = function(n) {
+	return partitions_specified_by(not_generators(n), n, n);
+}
+
+const main = function () {
+	let n = parseInt(document.getElementById('input').value);
+	document.getElementById('answer').innerHTML = dim_of_unoriented_cobordism_group(n);
+	document.getElementById('dim').innerHTML = n;
+}
